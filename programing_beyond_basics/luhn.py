ecbb250b247938ea18fcdd29c7e7c3819a6f38fb
oktavianu <oktavianuss.misro@gmail.com>
2025-01-02 09:14:55 +0700

    refactoring code

--- programing_beyond_basics/luhn.py
+++ programing_beyond_basics/luhn.py
@@ -1,1 +1,12 @@
-print("OK")
+def verify (digits):
+    total = 0
+    for i, d in enumerate(reversed(digits[:-1])): # enumerate in reversed order except the last digit, we slice it by [:-1]
+        x = int(d) * (2 - i % 2)
+        total += x // 10 + x % 10
+    return (int(digits[-1]) + total) % 10 == 0
+
+if __name__ == '__main__':
+    assert verify('17893729974')
+    assert not verify('17893729975')
+    print("ok")
+
