0d8dc4e5984d8ad52ad6ebfb23ae1d76a4065f09
oktavianu <oktavianuss.misro@gmail.com>
2025-01-10 15:06:40 +0700

    add luhn with lookup

---
+++ programing_beyond_basics/luhn_lookup.py
@@ -0,0 +1,16 @@
+LOOKUPS = (
+    dict(zip('0123456789', (0, 1, 2, 3, 4, 5, 6, 7, 8, 9))),
+    dict(zip('0123456789', (0, 2, 4, 6, 8, 1, 3, 5, 7, 9))),
+)
+
+def verify(digits):
+    return sum(LOOKUPS[i % 2][d] for i, d in enumerate(reversed(digits))) % 10 == 0
+
+if __name__ == "__main__":
+    try:
+        assert verify('17893729974')
+        assert not verify('17893729975')
+    except KeyError:
+        ...
+    print('ok')
+
