22d2208226e3ceff13175ce78b92874a911933bf
oktavianu <oktavianuss.misro@gmail.com>
2025-01-13 06:21:12 +0700

    updating

---
+++ programing_beyond_basics/tic-tac-toe/ttt.py
@@ -0,0 +1,18 @@
+"""
+Option 1:
+    - Model the state as the state of the board
+    - e.g. 3x3 array of arrays with player tokens for each item
+    - New moves are entered as (x, y) coords
+    - New moves are valid if array location is empty
+    - ...how to compute victory?
+    - Printing the board is basically printing the state
+    - Maybe we also store who is the current player
+    - Would we need to be able to undo move?
+    - We wouldn't be able to reply the game? Or save partial game in a database and continue?
+
+Option 2:
+    - Model the state as the sequence of moves
+    - 
+"""
+
+
