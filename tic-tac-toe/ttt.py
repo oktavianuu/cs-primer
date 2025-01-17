449db5364a90730ac74f5c9f0b6efb02df82f9b1
oktaa <oktavianuss.misro@gmail.com>
2025-01-17 21:32:02 +0700

    update tictactoe

--- tic-tac-toe/ttt.py
+++ tic-tac-toe/ttt.py
@@ -12,7 +12,26 @@ Option 1:
 
 Option 2:
     - Model the state as the sequence of moves
-    - 
-"""
+    - Derive the board current from the prior seq of moves
+    - Computing victory is comparably hard/easy
+    - A little more work to validate moves?
+    - If we want to support replaying ir undoing move etc, this will be straightforward
 
+We'll use option 2
+1). State is a sequence of moves in a python list
+2). Each move will be modeled as an integer between 0 and eight inclusive
+3). Validation: must be an integer in range that hasn't already been selected
+
+functions:
+    add_move(moves, m) --> True/False
+        - validate the move
+        - append m to moves
+    check_winner(moves) -> 0, 1 or None
+        - deinterleave played move into those played by player 0 and player 1 e.g. [0, 9, 1, 2] would become [0, 1, 2] and [9, 8]
+    print_board(moves)
+        - reformat 1 x 9 list as 3 x 3 grid, for printing
+        - have default items of A to I, so that player can select which square to play
+        - for each move that has been made, overwrite the placeholder ketter with x or 0
+    next_player(moves)
+        -len(moves) % 2
 
